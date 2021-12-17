3f3f9dfb0df8e47e7611c2fb9eabf2f63880cfc4
fifty-six <ybham6@gmail.com>
2021-12-16 20:52:37 -0500

    add path to string

--- src/main.zig
+++ src/main.zig
@@ -14,8 +14,10 @@ const Output = output.Output;
 const Menu = menus.Menu;
 const MenuEntry = menus.MenuEntry;
 
+const utf16_str = std.unicode.utf8ToUtf16LeStringLiteral;
+
 var alloc_bytes: [100 * 1024]u8 = undefined;
-var alloc_state = std.heap.FixedBufferAllocator.init(&panic_allocator_bytes);
+var alloc_state = std.heap.FixedBufferAllocator.init(&alloc_bytes);
 const fixed_alloc = &alloc_state.allocator;
 
 var sys_table: *uefi.tables.SystemTable = undefined;
@@ -23,6 +25,34 @@ var boot_services: *uefi.tables.BootServices = undefined;
 var con_in: *protocols.SimpleTextInputProtocol = undefined;
 var con_out: *protocols.SimpleTextOutputProtocol = undefined;
 var out: Output = undefined;
+
+const tag_to_utf16_literal = init: {
+    @setEvalBranchQuota(10000);
+    const KV = struct { @"0": []const u8, @"1": []const u16 };
+
+    comptime var enums = [_]type{
+        protocols.MediaDevicePath.Subtype,
+        protocols.HardwareDevicePath.Subtype,
+        protocols.DevicePathType,
+    };
+
+    comptime var kv: []const KV = &.{};
+
+    inline for (enums) |e| {
+        comptime var fields = @typeInfo(e).Enum.fields;
+
+        inline for (fields) |field| {
+            kv = kv ++ &[_]KV{.{
+                .@"0" = field.name,
+                .@"1" = utf16_str(field.name),
+            }};
+        }
+    }
+
+    comptime var map = std.ComptimeStringMap([]const u16, kv);
+
+    break :init map;
+};
 
 pub fn open_protocol(handle: uefi.Handle, comptime protocol: type) !*protocol {
     var ptr: *protocol = undefined;
@@ -46,6 +76,48 @@ const FullDevicePathErr = error{
     UnknownGlyph,
 };
 
+pub fn device_path_to_str(alloc: *std.mem.Allocator, dpp: *protocols.DevicePathProtocol) ![:0]u16 {
+    var res = std.ArrayList(u16).init(alloc);
+    errdefer res.deinit();
+
+    var utf16: [256]u16 = undefined;
+
+    var node = dpp;
+    while (node.type != .End) {
+        var path = node.getDevicePath() orelse return error.NoDevicePath;
+
+        switch (path) {
+            .Hardware => |hw| {
+                try res.appendSlice(tag_to_utf16_literal.get(@tagName(hw)).?);
+            },
+            .Media => |m| {
+                switch (m) {
+                    .FilePath => |fp| {
+                        try res.appendSlice(std.mem.span(fp.getPath()));
+                    },
+                    else => {
+                        try res.appendSlice(tag_to_utf16_literal.get(@tagName(m)).?);
+                    },
+                }
+            },
+            .Messaging, .Acpi => {
+                // TODO: upstream
+            },
+            // Adding a backslash at the end anyways.
+            .End => {},
+            else => {
+                try res.append('?');
+            },
+        }
+
+        try res.append('\\');
+
+        node = @intToPtr(@TypeOf(node), @ptrToInt(node) + node.length);
+    }
+
+    return try res.toOwnedSliceSentinel(0);
+}
+
 pub fn full_device_path(dpp: *protocols.DevicePathProtocol) anyerror!void {
     if (dpp.type == .Media) {
         var subtype = @intToEnum(protocols.MediaDevicePath.Subtype, dpp.subtype);
@@ -56,7 +128,7 @@ pub fn full_device_path(dpp: *protocols.DevicePathProtocol) anyerror!void {
             try out.printf("Unhandled .Media {s}\r\n", .{@tagName(subtype)});
         }
     } else {
-        try out.printf("Node: {s}\r\n", .{@tagName(dpp.type)});
+        try out.printf("Node: {d}\r\n", .{dpp.type});
     }
 
     if (dpp.type == .End) {
@@ -65,7 +137,8 @@ pub fn full_device_path(dpp: *protocols.DevicePathProtocol) anyerror!void {
         return;
     }
 
-    var len = 4 + @intCast(u8, dpp.length >> 8);
+    // var len = 4 + @intCast(u8, dpp.length >> 8);
+    var len = dpp.length;
 
     try out.printf("Length: {d}\r\n", .{len});
 
@@ -108,7 +181,8 @@ pub fn caught_main() !void {
     if (@intToEnum(protocols.MediaDevicePath.Subtype, sfp.file_path.subtype) != .FilePath)
         return error.DeviceHandleNotAtFilePath;
 
-    try full_device_path(sfp.file_path);
+    try out.print16(try device_path_to_str(fixed_alloc, sfp.file_path));
+    // full_device_path(sfp.file_path);
     // try out.print16(sfp.file_path.getDevicePath().?.Media.FilePath.getPath());
     // try out.print("\r\n");
 
